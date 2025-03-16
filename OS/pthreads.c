dffbbc98f72aba72b41f46790adb9e474aa6866e
shanmukharoczzs <shanmukharoczzs@gmail.com>
2025-03-16 12:31:35 +0530

    OS-codes

---
+++ OS/pthreads.c
@@ -0,0 +1,44 @@
+/*
+ *            Author : Shannu
+ *            Topic: Pthreads
+ *            Date   : 15 - March - 2025
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define NUM_THREADS 5
+
+// Function that will be executed by each thread
+void *print_hello(void *threadid) {
+    long tid = (long)threadid;
+    printf("Hello from thread %ld!\n", tid);
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+
+    // Create threads
+    for (t = 0; t < NUM_THREADS; t++) {
+        printf("Creating thread %ld\n", t);
+        rc = pthread_create(&threads[t], NULL, print_hello, (void *)t);
+        if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
+
+    // Wait for all threads to complete
+    for (t = 0; t < NUM_THREADS; t++) {
+        pthread_join(threads[t], NULL);
+    }
+
+    printf("All threads completed.\n");
+
+    // Exit the main thread
+    pthread_exit(NULL);
+}
