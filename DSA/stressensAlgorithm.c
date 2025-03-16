e34cf3012e341596773c028f6842600891e6a9f0
shanmukharoczzs <shanmukharoczzs@gmail.com>
2025-03-16 11:35:38 +0530

    changed

--- DSA/stressensAlgorithm.c
+++ DSA/stressensAlgorithm.c
@@ -6,9 +6,30 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
+
+int stressen(int a, int b) {
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+    if (a == b)
+        return a;
+    if (a % 2 == 0) {
+        if (b % 2 == 0)
+            return stressen(a / 2, b / 2);
+        else
+            return stressen(a / 2, (b - 1) / 2);
+    }
+    if (a > b)
+        return stressen((a - b), b);
+    else
+        return stressen((b - a), a);
+}
 
 int main() {
-    
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("GCD of %d and %d is %d\n", a, b, stressen(a, b));
     return 0;
 }
